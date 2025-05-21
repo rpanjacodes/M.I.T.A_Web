96400a5d7d1ee133cdbc4a0912de77b19274749c
r.panja codes <101450382+rpanjacodes@users.noreply.github.com>
2025-05-21 11:39:26 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+  botName: "M.I.T.A Bot",
+  botAvatar: "https://upload.wikimedia.org/wikipedia/en/4/47/Hyouka_eru.png",
+  inviteURL: "https://discord.com/oauth2/authorize?client_id=YOUR_CLIENT_ID&scope=bot",
+  supportURL: "https://discord.gg/YOUR_SUPPORT_INVITE"
+};
