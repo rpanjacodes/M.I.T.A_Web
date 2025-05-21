c2528249ab52fcc72a906e563de8e1de646fa3a9
r.panja codes <101450382+rpanjacodes@users.noreply.github.com>
2025-05-21 12:03:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   botName: "M.I.T.A Bot",
-  botAvatar: "https://upload.wikimedia.org/wikipedia/en/4/47/Hyouka_eru.png",
-  inviteURL: "https://discord.com/oauth2/authorize?client_id=YOUR_CLIENT_ID&scope=bot",
-  supportURL: "https://discord.gg/YOUR_SUPPORT_INVITE"
+  botAvatar: "https://cdn.discordapp.com/avatars/1342170248022986866/4a4c6a473762b2a61bb17ee7c1598214.png?size=1024",
+  inviteURL: "https://discord.com/oauth2/authorize?client_id=1342170248022986866&permissions=268823880&scope=bot+applications.commands",
+  supportURL: "https://discord.gg/kd3f6t8DyN"
 };
